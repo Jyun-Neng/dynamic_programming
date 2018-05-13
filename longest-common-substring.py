d34062ec694fbd7aa48285c0b25b66ef875c3b8e
Jyun-NengJi <jyunnengji@gmail.com>
2018-05-13 22:49:11 +0800

    reduce two arrays to one array only

--- longest-common-substring.py
+++ longest-common-substring.py
@@ -1,51 +1,49 @@
 # Give two strings. Find the longest common substring
-def lcsLength(str1, str2):
+def lcs(str1, str2):
     """
-    Build the direct table,
-    and count the characters. 
-    There are two situations:
-    1. if str1[x] == str2[y], then compare last characters str1[x-1] and str2[y-1]. 
-    2. if str1[x] != str2[y], then compare str1[x-1] and str2[y] or str1[x] and str2[y-1].
+    Find the longest common substring in str1 and str2. 
+    Time complexity is O(mn), where m and n are string length of str1 and str2, respectively. 
+    And it need to create a 2-D array 'cnt'.
+    The auxiliary space is big-theta(mn).
     """
     str1_len = len(str1)
     str2_len = len(str2)
+    # space complexity is big-theta(mn)
     cnt = [[0 for x in range(str1_len + 1)] for y in range(str2_len + 1)]
-    direct = [['none' for x in range(str1_len + 1)]
-              for y in range(str2_len + 1)]
 
-    for x in range(1, str1_len + 1):
-        for y in range(1, str2_len + 1):
-            if str1[x - 1] == str2[y - 1]:
+    # calculate the length of LCS
+    # time complexity is O(mn)
+    for y in range(str2_len + 1):
+        for x in range(str1_len + 1):
+            if x == 0 or y == 0:
+                cnt[y][x] = 0
+            elif str1[x - 1] == str2[y - 1]:
                 cnt[y][x] = cnt[y - 1][x - 1] + 1
-                direct[y][x] = 'upper-left'
-            # direct to the largest cnt value
-            elif cnt[y - 1][x] >= cnt[y][x - 1]:
-                cnt[y][x] = cnt[y - 1][x]
-                direct[y][x] = 'up'
             else:
-                cnt[y][x] = cnt[y][x - 1]
-                direct[y][x] = 'left'
+                cnt[y][x] = max(cnt[y - 1][x], cnt[y][x - 1]) # record the optimize solution
 
-    return cnt, direct
+    x, y = str1_len, str2_len
+    lcs_num = cnt[y][x]
+    lcs = [''] * lcs_num
 
+    # print the LCS
+    # time complexity is O(m+n)
+    while x > 0 and y > 0:
+        if str1[x - 1] == str2[y - 1]:
+            lcs[lcs_num - 1] = str1[x - 1]
+            lcs_num -= 1
+            x -= 1
+            y -= 1
+        elif cnt[y - 1][x] >= cnt[y][x - 1]: # optimize solution
+            y -= 1
+        else:
+            x -= 1
 
-def printLCS(direct, str1, x, y):
-    """
-    Through the direct table, show the LCS. 
-    """
-    if x == 0 or y == 0:
-        return
-    elif direct[y][x] == 'upper-left':  # characters match
-        printLCS(direct, str1, x - 1, y - 1)
-        print(str1[x - 1], end='')
-    elif direct[y][x] == 'up':
-        printLCS(direct, str1, x, y - 1)
-    else:
-        printLCS(direct, str1, x - 1, y)
+    return lcs
 
 
 if __name__ == '__main__':
-    str1 = 'geeksforgeek'
-    str2 = 'geeksabcgeek'
-    cnt, direct = lcsLength(str1, str2)
-    printLCS(direct, str1, len(str1), len(str2))
+    str1 = 'geeksf21wfaisj'
+    str2 = 'geeksabcgeeksadgwirwe'
+    lcsstr = lcs(str1, str2)
+    print(''.join(lcsstr))
