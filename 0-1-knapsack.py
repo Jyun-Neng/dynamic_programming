200b46e365ee9b7c7bd7fd5660f2c053e225e48f
Jyun-NengJi <jyunnengji@gmail.com>
2018-05-27 21:32:38 +0800

    autopep8

--- 0-1-knapsack.py
+++ 0-1-knapsack.py
@@ -4,25 +4,26 @@ def knapsack(capacity, value, weight):
     finite capacity W to get the maximum total value in the knapsack.
     """
     # space complexity is O(W)
-    K = [[0 for x in range(capacity+1)] for x in range(2)]
+    K = [[0 for x in range(capacity + 1)] for x in range(2)]
     s = [0] * 2
     # build table
     # time complexity is O(nW)
-    for i in range(len(value)+1):
-        for c in range(capacity+1):
-            if i==0 or c==0:
-                K[i%2][c] = 0
-            elif weight[i-1] <= c:
-                if value[i-1] + K[(i-1)%2][c-weight[i-1]] >  K[(i-1)%2][c]:
-                    K[i%2][c] = value[i-1] + K[(i-1)%2][c-weight[i-1]]
-                    s[c%2] = i
-                else:    
-                    K[i%2][c] = K[(i-1)%2][c]
-                    s[c%2] = s[(c-1)%2]
+    for i in range(len(value) + 1):
+        for c in range(capacity + 1):
+            if i == 0 or c == 0:
+                K[i % 2][c] = 0
+            elif weight[i - 1] <= c:
+                if value[i - 1] + K[(i - 1) % 2][c - weight[i - 1]] > K[(i - 1) % 2][c]:
+                    K[i % 2][c] = value[i - 1] + \
+                        K[(i - 1) % 2][c - weight[i - 1]]
+                    s[c % 2] = i
+                else:
+                    K[i % 2][c] = K[(i - 1) % 2][c]
+                    s[c % 2] = s[(c - 1) % 2]
             else:
-                K[i%2][c] = K[(i-1)%2][c]
+                K[i % 2][c] = K[(i - 1) % 2][c]
 
-    return K[len(value)%2][capacity], s[capacity%2]
+    return K[len(value) % 2][capacity], s[capacity % 2]
 
 
 if __name__ == '__main__':
